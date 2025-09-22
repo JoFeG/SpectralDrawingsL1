d102657f551abe4069f8ebacf861bb6014a8ef51
JoFeG <jose.fernandezg@umag.cl>
2025-09-22 18:28:54 -0300

    notebook example

--- test/TEST0_loading_and_ploting.jl
+++ test/TEST0_loading_and_ploting.jl
@@ -8,5 +8,11 @@ G = SimpleGraph{Int64}(A)
 AEIG = eigen(A)
 #display(AEIG)
 
-X = AEIG.vectors[:,3:4]
-GraphVectsPlot(G, X)
+X = AEIG.vectors[:,2:4]
+#GraphVectsPlot(G, X)
+
+anim = @animate for i=1:36
+    plot(camera = (10i,30))
+    GraphVectsPlot(G, X)
+end
+gif(anim, "./out/graph_442.gif", fps = 8)
