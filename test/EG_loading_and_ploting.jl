ee7ec4cf822db3c6fa8f7c9ffd59fc558ba4b538
JoFeG <jose.fernandezg@umag.cl>
2025-09-22 18:06:07 -0300

    W_5 load and plot test

---
+++ test/EG_loading_and_ploting.jl
@@ -0,0 +1,12 @@
+using LinearAlgebra, Graphs, Plots, DelimitedFiles
+include("../src/spectral_drawing_utils.jl")
+
+A = readdlm("./data/graph_442.mat", Int64)
+n = size(A)[1]
+G = SimpleGraph{Int64}(A)
+
+AEIG = eigen(A)
+display(AEIG)
+
+X = AEIG.vectors[:,3:4]
+GraphVectsPlot(G, X)
