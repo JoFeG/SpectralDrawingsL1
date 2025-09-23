f01b74483dd00a4c3e6264b9990d2b16f0c00a13
JoFeG <jose.fernandezg@umag.cl>
2025-09-23 15:57:11 -0300

    b(G) brute force computation

--- src/spectral_drawing_utils.jl
+++ src/spectral_drawing_utils.jl
@@ -1,3 +1,5 @@
+using Graphs, Plots
+
 function GraphVectsPlot(G,X)
     n, d = size(X)
     plt = plot!(
@@ -17,3 +19,37 @@ function GraphVectsPlot(G,X)
     end
     return plt
 end
+
+
+function bBruteForce1(G)
+    n = nv(G)
+    M = Inf
+    for k = 1:3^n
+        S1S2 = [mod(k÷3^i,3) for i=0:n-1]
+        S1 = Int.(S1S2 .== 1)
+        sum(S1) > 0 ? ξ1 = karger_cut_cost(G,S1)/sum(S1) : ξ1 = Inf
+        S2 = Int.(S1S2 .== 2)
+        sum(S2) > 0 ? ξ2 = karger_cut_cost(G,S2)/sum(S2) : ξ2 = Inf
+        M = min(M,(ξ1 + ξ2)/2)
+    end
+    return M
+end
+
+function L1VectorsBruteForce(G)
+    bG = bBruteForce1(G)
+    n = nv(G)
+    X = Array{Float64}(undef, n, 0)
+    for k = 1:3^n
+        S1S2 = [mod(k÷3^i,3) for i=0:n-1]
+        S1 = Int.(S1S2 .== 1)
+        sum(S1) > 0 ? ξ1 = karger_cut_cost(G,S1)/sum(S1) : ξ1 = Inf
+        S2 = Int.(S1S2 .== 2)
+        sum(S2) > 0 ? ξ2 = karger_cut_cost(G,S2)/sum(S2) : ξ2 = Inf
+        if bG == (ξ1 + ξ2)/2
+            V = S1 ./ 2sum(S1) - S2 ./ 2sum(S2) 
+            X = cat(X,V,dims=2)
+        end
+    end
+    return bG, X
+end
+   
